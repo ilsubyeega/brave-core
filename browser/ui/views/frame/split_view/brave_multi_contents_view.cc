aa256e7229f9760d7f115e66b3af8e69f875caac
Simon Hong <shong@brave.com>
2025-06-05 17:16:11 +0900

    Fixed crash when swap split tabs
    Crash happened because SecurityStateTabHelper was null for inactive tab
    in split tabs. It should not be null because split view location bar
    fetches tab's current security status indirectly via ChromeLocationBarModelDelegate.
    
    So far, BraveMultiContentsView calls UpdateSecondaryLocationBar() whenever SetActiveIndex()
    is called. If SetActiveIndex() is called after proper WebContents are set on both inactive/active
    web view in split tabs, there is no crash. However, SetActiveIndex() could be called if one of them is cleared.
    At BrowserView::OnSplitTabContentsUpdated(), that can happen.
    So, WebView::GetWebContents() created another WebContents accidently and that created WebContents doesn't have
    SecurityStateTabHelper.
    
    To fix this, made UpdateSecondaryLocationBar() after proper WebContents are set on inactive/active webview in split tabs.

--- browser/ui/views/frame/split_view/brave_multi_contents_view.cc
+++ browser/ui/views/frame/split_view/brave_multi_contents_view.cc
@@ -25,6 +25,12 @@ namespace {
 constexpr auto kSpacingBetweenContentsWebViews = 4;
 }  // namespace
 
+// static
+BraveMultiContentsView* BraveMultiContentsView::From(MultiContentsView* view) {
+  CHECK(view);
+  return static_cast<BraveMultiContentsView*>(view);
+}
+
 BraveMultiContentsView::BraveMultiContentsView(
     BrowserView* browser_view,
     WebContentsFocusedCallback inactive_contents_focused_callback,
@@ -111,12 +117,6 @@ void BraveMultiContentsView::Layout(PassKey) {
   contents_container_views_[1]->SetBoundsRect(end_rect);
 }
 
-void BraveMultiContentsView::SetActiveIndex(int index) {
-  MultiContentsView::SetActiveIndex(index);
-
-  UpdateSecondaryLocationBar();
-}
-
 float BraveMultiContentsView::GetCornerRadius() const {
   return BraveBrowser::ShouldUseBraveWebViewRoundedCorners(
              browser_view_->browser())
@@ -144,7 +144,7 @@ void BraveMultiContentsView::UpdateSecondaryLocationBar() {
   // as it's attached to inactive contents view.
   int inactive_index = active_index_ == 0 ? 1 : 0;
   secondary_location_bar_->SetWebContents(
-      GetInactiveContentsView()->GetWebContents());
+      GetInactiveContentsView()->web_contents());
   secondary_location_bar_->SetParentWebView(
       contents_container_views_[inactive_index]);
 
